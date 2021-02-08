0233e4679ddcbbc8869edda6c503119fa6579aef
Jose Mendoza <pball626@gmail.com>
2021-02-08 16:28:46 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,10 +53,8 @@ def add_characters
          ]
       }
    }
-epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
-epic_tragedy[:additional_characters] << { name: "Apothecary" }
-  prince = {name: "Prince Escalus"}
-  apothecary = { name: "Apothecary"}
+epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
+epic_tragedy[:additional_characters][0] = { name: "Apothecary" }
   #epic_tragedy[:additional_characters] << prince
   #epic_tragedy[:additional_characters] << apothecary
    # Write code below that does the following programmatically:
