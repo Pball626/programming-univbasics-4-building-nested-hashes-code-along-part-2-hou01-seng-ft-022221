dd35caca206b6c224bd8a9c1f8a6bfbfdeafd9d1
Jose Mendoza <pball626@gmail.com>
2021-02-08 16:26:14 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,11 +53,12 @@ def add_characters
          ]
       }
    }
-epic_tragedy[:additional_characters] = []
+epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+epic_tragedy[:additional_characters] << { name: "Apothecary" }
   prince = {name: "Prince Escalus"}
   apothecary = { name: "Apothecary"}
-  epic_tragedy[:additional_characters] << prince
-  epic_tragedy[:additional_characters] << apothecary
+  #epic_tragedy[:additional_characters] << prince
+  #epic_tragedy[:additional_characters] << apothecary
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
