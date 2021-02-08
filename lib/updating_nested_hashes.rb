7069e0e7d7ed65d09d39cab2f726dfae8ddbf465
Jose Mendoza <pball626@gmail.com>
2021-02-08 16:32:04 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,6 +53,8 @@ def add_characters
          ]
       }
    }
+{ :name => "Prince Escalus" }
+{ :name => "Apothecary" }
 epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
 epic_tragedy[:additional_characters][1] = { name: "Apothecary" }
   #epic_tragedy[:additional_characters] << prince
